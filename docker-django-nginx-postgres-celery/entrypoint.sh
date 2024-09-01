baa6d4fdd03761b06c1098aeb6dafcea62b2c8df
dianamatkava <dianamatkava@gmail.com>
2024-09-01 19:37:49 +0200

    entrypoint

--- docker-django-nginx-postgres-celery/entrypoint.sh
+++ docker-django-nginx-postgres-celery/entrypoint.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
+# Wait for Postgres to be ready
+if [ "$DATABASE" = "postgres" ]; then
+    echo "Waiting for Postgres..."
     while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
+        sleep 0.1
     done
-
-    echo "PostgreSQL started"
-    python manage.py runserver
+    echo "Postgres started"
 fi
 
+# Run the main command
 exec "$@"
